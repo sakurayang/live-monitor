c5157fa65ba4f1a5e10548ab33ed72e709ddbeb2
sakurayang <1304274443@qq.com>
2020-01-22 02:32:16 +0800

    rebuilding

--- src/components/Chart.vue
+++ src/components/Chart.vue
@@ -1,111 +1,17 @@
 <template>
-  <div id="chart"></div>
+  <div id="chart" />
 </template>
 
 <script>
-import echarts from "../utils/echarts";
-import config from "@/utils/config";
-import axios from "axios";
 export default {
   name: "Chart",
   props: {
     // eslint-disable-next-line vue/require-default-prop
     id: Number
-  },
-  data: async function() {},
-  created: async function() {
-    let live_status = Number((await axios.get(config.api + "/living")).data);
-    let live_data = live_status ? (await axios.get(config.api)).data : [];
-    let start =
-      live_data.length > 51 ? (1 - 50 / (live_data.length - 1)) * 100 : 0;
-    this.chart_options = {
-      color: [config.color[this.id]],
-      title: {
-        text: `【${this.id}】`
-      },
-      grid: {
-        tooltip: {
-          show: true,
-          tigger: "axis",
-          axisPointer: { type: "cross" },
-          snap: true
-        },
-        axisPointer: {
-          link: [{ xAxisIndex: "all" }],
-          snap: true
-        }
-      },
-      legend: { top: 10 },
-      toolbox: {
-        feature: {
-          saveAsImage: { show: true },
-          restore: { show: true },
-          dataView: { show: false },
-          dataZoom: { show: false },
-          magicType: { show: false },
-          brush: { show: false }
-        }
-      },
-      dataZoom: [
-        {
-          xAxisIndex: [0],
-          type: "slider",
-          start,
-          end: 100
-        },
-        {
-          xAxisIndex: [0],
-          type: "inside",
-          start,
-          end: 100
-        },
-        {
-          yAxisIndex: [0],
-          type: "slider",
-          start: 0,
-          end: 100
-        }
-      ],
-      xAxis: {
-        type: "time",
-        interval: 5 * 1000 //5second * 1000ms
-      },
-      yAxis: [{ type: "value" }],
-      series: [{ type: "line" }],
-      dataset: {
-        dimensions: ["time", "count"],
-        source: []
-      }
-    };
-  },
-  mounted: async function() {
-    this.$nextTick(async function() {
-      let live_data = await axios.get(config.api + "/init");
-      live_data = live_data.data.code == 0 ? live_data.data.result : [];
-      let live_status = await Number(
-        (await axios.get(config.api + "/living")).data
-      );
-      this.chart = echarts.init(document.getElementById("chart"));
-      this.chart.init(document.getElementById("chart"));
-      let start =
-        live_data.length > 51 ? (1 - 50 / (live_data.length - 1)) * 100 : 0;
-      this.chart_options.dataZoom[0].start = start;
-      this.chart_options.dataZoom[1].start = start;
-      this.chart_options.dataset.source = live_data;
-      live_status ? this.chart.setOption(this.chart_options) : alert("未开播");
-    });
-  },
-  methods: {
-    updateInfo: async function() {
-      let live_status = Number((await axios.get(config.api + "/living")).data);
-      let live_data = live_status ? (await axios.get(config.api)).data : [];
-      return {
-        live_status,
-        live_data
-      };
-    },
-    updateChart: async function() {}
   }
 };
 </script>
-<style scoped></style>
+
+<style>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,30 @@
 import Vue from "vue";
-import Route from "vue-route";
-import Index from "@/views/Index.vue";
+import VueRouter from "vue-router";
+import Home from "../views/Home.vue";
 
-Vue.use(Route);
+Vue.use(VueRouter);
 
 const routes = [
   {
     path: "/",
-    component: Index
+    name: "home",
+    component: Home
   },
   {
-    path: "/:id",
-    component: () => import("@/views/chart.vue"),
-    props: true
-  },
-  {
-    path: "*",
-    redirect: "/"
+    path: "/about",
+    name: "about",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/About.vue")
   }
 ];
 
-const router = new Route({ mode: "history", routes });
-
-// router.beforeEach();
+const router = new VueRouter({
+  mode: "history",
+  base: process.env.BASE_URL,
+  routes
+});
 
 export default router;
