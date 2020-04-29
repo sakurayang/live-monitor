e94421fdb2bacc658ca8cbc9cb041064a19b1d3a
sakurayang <1304274443@qq.com>
2020-04-29 15:22:55 +0800

    change font-awasome from components to svg

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import VueNoty from "vuejs-noty";
-import VueAxiosPlugin from "vue-axios-plugin";
 
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { fas } from "@fortawesome/free-solid-svg-icons";
@@ -13,15 +12,6 @@ import "vuejs-noty/dist/vuejs-noty.css";
 library.add(fas);
 
 Vue.use(VueNoty);
-
-Vue.use(VueAxiosPlugin, {
-  // 请求拦截处理
-  reqHandleFunc: config => config,
-  reqErrorFunc: error => Promise.reject(error),
-  // 响应拦截处理
-  resHandleFunc: response => response,
-  resErrorFunc: error => Promise.reject(error)
-});
 
 Vue.component("font-awesome-icon", FontAwesomeIcon);
 Vue.config.productionTip = false;
