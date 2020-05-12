b90f0f6525b9f6f605c55a0f7fbfad26e9439f90
sakurayang <1304274443@qq.com>
2020-05-12 23:01:22 +0800

    make config can edit in frontend

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,7 +1,78 @@
-let config = {
-  api: "http://114.55.100.200:3000",
-  color: {
-    "21618129": "#e74064"
-  }
+let value = {
+  api: "http://114.55.100.200:8083"
 };
-export default config;
+let default_value = { ...value };
+Object.freeze(default_value);
+
+// put default config in localstorage
+localStorage.setItem("default_config", JSON.stringify(default_value));
+// put config in localstorage
+localStorage.setItem("config", JSON.stringify(value));
+
+/**
+ * search key in config.
+ * return true if has, overwise false.
+ * @param {String} key
+ * @param {JSON} search_object
+ * @return {Boolean}
+ */
+function has(key, search_object = value) {
+  if (typeof search_object !== "object") return false;
+  return search_object.hasOwnProperty(key);
+}
+
+/**
+ * set config.
+ * return true if succsed, overwise false.
+ * @param {String} key
+ * @param {*} value
+ * @return {Boolean}
+ */
+function set(key, insert_value) {
+  value[key] = insert_value;
+  localStorage.setItem("config", JSON.stringify(value));
+  return (
+    value[key] === insert_value &&
+    JSON.parse(localStorage.getItem("config"))[key] === insert_value
+  );
+}
+
+/**
+ * get value form config.
+ * return value if has, overwise null.
+ * @param {String} key
+ * @return {*}
+ */
+function get(key) {
+  return has(key, value) ? value[key] : null;
+}
+
+/**
+ * delete item
+ * @param {String} key
+ * @return {Boolean} true if succsed, false if key not found or faild
+ */
+function _delete(key) {
+  if (has(key, value)) {
+    localStorage.removeItem("config");
+    let result = delete value[key];
+    localStorage.setItem("config", JSON.stringify(value));
+    return result;
+  } else {
+    return false;
+  }
+}
+
+/**
+ * restore config
+ * @return {Void}
+ */
+function restore() {
+  value = {};
+  localStorage.removeItem("config");
+  value = { ...default_value };
+  localStorage.setItem("config", JSON.stringify(default_value));
+}
+
+export let config = { get, set, has, delete: _delete, restore };
+export default value;
